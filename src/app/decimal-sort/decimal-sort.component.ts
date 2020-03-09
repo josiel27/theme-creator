45bf9eadf1c0b68370286d56d2aa7b739d82fe57
JOSIEL DA SILVA FARIA <josiel.faria-ext@viavarejo.com.br>
2020-03-09 18:08:53 -0300

    Adicionando logica para numeros randomicos hexdecimais

--- src/app/decimal-sort/decimal-sort.component.ts
+++ src/app/decimal-sort/decimal-sort.component.ts
@@ -6,10 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./decimal-sort.component.scss']
 })
 export class DecimalSortComponent implements OnInit {
-
+  numberAtual: any = 0;
+  maxNumber = 10000000000000000000000000010000000000000000000000000010000000000000000000000100000000000000000000000000100000000000000000000;
+  minNumber = 0;
+  numberHex = '';
+  increment = 10000000000000000000000000010000000000000000000000000010000000000000000000000;
   constructor() { }
 
   ngOnInit() {
+    this.numberAtual = this.decimalToHex(this.numberAtual);
+  }
+
+  addNumber() {
+    (this.numberAtual < this.maxNumber)
+      ? this.numberAtual += this.increment
+      : this.numberAtual = this.minNumber;
+
+    this.decimalToHex(this.numberAtual);
+  }
+
+  rmvNumber() {
+    (this.numberAtual > this.minNumber)
+      ? this.numberAtual -= this.increment
+      : this.numberAtual = this.maxNumber;
+
+    this.decimalToHex(this.numberAtual);
+  }
+
+  decimalToHex(numb: number) {
+    let hex = numb.toString(16);
+    hex = '000000'.substr(0, 6 - hex.length) + hex;
+    this.numberHex = '#' + hex.substr(0, 6);
+    console.log(hex);
+  }
+
+  randomInt(min, max) {
+    return min + Math.floor((max - min) * Math.random());
   }
 
 }
