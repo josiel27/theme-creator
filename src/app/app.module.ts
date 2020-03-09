c6889e8fed66fde7b2492aeaa88980c1bc8525c0
JOSIEL DA SILVA FARIA <josiel.faria-ext@viavarejo.com.br>
2020-03-09 16:43:33 -0300

    Adicionando componente decimal sort no modulo principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { DecimalSortComponent } from './decimal-sort/decimal-sort.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    DecimalSortComponent
   ],
   imports: [
     BrowserModule,
