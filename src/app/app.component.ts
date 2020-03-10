bb68ef86b64979575e3a55f5ba5cd167f7460b10
JOSIEL DA SILVA FARIA <josiel.faria-ext@viavarejo.com.br>
2020-03-10 16:54:43 -0300

    Adicionando funcao para mostrar o componente selecionado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,18 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  componentSelected: string;
+  showComponent = '';
+
+  activeComponentSelected(componentSelected) {
+    console.log('saida ', componentSelected);
+    const showComponent = {
+      'decimal-sort': 'decimal-sort',
+      'colors-hexdecimal': 'colors-hexdecimal'
+    }[componentSelected.id];
+
+    this.showComponent = showComponent;
+  }
+}
+
